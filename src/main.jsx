19ff0f849b1b6c7edb51c6693615f4dec9a4076e
sunryeyoo <sunryeyoo@gmail.com>
2026-04-01 16:47:01 +0900

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./assets/css/style.css";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
 
 import App from "./App.jsx";
 
